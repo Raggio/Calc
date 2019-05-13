808a1a2c128712d3865bfe10e2a417c1312f8ff9
Dennis <denissd98@hotmail.com>
2019-05-13 12:16:08 +0200

    Crear calc

---
+++ AllTests.java
@@ -0,0 +1,11 @@
+package Calc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Suma.class, Resta.class, Multiplicacion.class,Divion.class})
+public class AllTests {
+
+}
--- MainTest.java
+++ MainTest.java
@@ -6,68 +6,33 @@ import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
+import entornossuma.Suma;
+
 class MainTest {
 
 	
 	
+	
+	int numero1;
+	int numero2;
+		
+	public Suma(int numero1,int numero2) {
+		this.numero1=numero1;
+		this.numero2=numero2;
+		
+		}
 	@Test
-	public static void main(String[] args) {
-		int numero1;
-		int numero2;
+	public int Sumar() {
+		int resultado;
+		resultado=this.numero1+this.numero2;
+		System.out.println("El resultado es " + resultado);	
 		
+		Sumar();
+		assertTrue(.Sumar(2,3)==5.0);
+		return resultado;
 		
-		System.out.println("Introduce el primer numero");
-		Scanner sc=new Scanner(System.in);
-		numero1=sc.nextInt();
-		System.out.println("Elije operación:");
-		System.out.println("1-- Sumar");
-		System.out.println("2-- Restar");
-		System.out.println("3-- Multiplicar");
-		System.out.println("4-- Dividir");
-		
-		int operacion=sc.nextInt();
-		switch (operacion) {
-		case 1: System.out.println("Introduce el valor a Sumar");
-				numero2=sc.nextInt();
-				Suma suma=new Suma(numero1,numero2);
-				suma.Sumar();
-			
-			break;
-		case 2: System.out.println("Introduce valor a restar");
-				numero2=sc.nextInt();
-				Resta resta=new Resta(numero1,numero2);
-				resta.Restar();
-			break;	
-		case 3: System.out.println("Introduce valor a multiplicar");
-				numero2=sc.nextInt();
-				Multiplicacion multiplicacion=new Multiplicacion(numero1,numero2);
-				multiplicacion.Multiplicar();
-			break;
-		case 4: System.out.println("Introduce valor a dividir");
-				numero2=sc.nextInt();
-				Division division=new Division(numero1, numero2);
-				division.Dividir();
-			break;
-		
-		
-		
-		default: System.out.println("Introduce un numero");
-			break;
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-		}
+}
 		
 		
 		
