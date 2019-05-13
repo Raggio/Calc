7c8f3fc1b33765dfb6759af5bf17afa03b97cf33
Dennis <denissd98@hotmail.com>
2019-05-13 12:56:02 +0200

    aAsd
    asd

--- AllTests.java
+++ AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({Suma.class, Resta.class, Multiplicacion.class,Divion.class})
+@SuiteClasses({Suma.class, Resta.class, Multiplicacion.class,Division.class})
 public class AllTests {
 
 }
